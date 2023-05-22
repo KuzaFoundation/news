96154a1667ed6b3743c19e0c399cf678e254defd
KuzaFoundation <132609592+KuzaFoundation@users.noreply.github.com>
2023-05-22 16:01:38 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/", function(req,res) {
+  res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/", function(req, res){
+
+  var firstName = req.body.fname;
+  var lastName = req.body.lname;
+  var email = req.body.email
+
+  console.log(firstName, lastName, email);
+
+  if (res.statusCode === 200){
+    res.sendFile(__dirname + "/success.html")
+  }
+  else {
+    res.sendFile(__dirname + "/failue.html")
+  }
+
+})
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("server is running on port 3000");
+})
